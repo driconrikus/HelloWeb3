0572c1cf3da4e70a6e00e54efd0d0eed5faeac9a
Ricardo Valdez <rickondrius@gmail.com>
2025-03-15 21:02:14 -0400

    Added more variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,22 @@
 variable "region" {
     description = "Region in which GCP resources will be created"
     type        = string
+    default     = "us-central1"
 }
 
 variable "project_id" {
-    description = "Project Name"
+    description = "Project ID"
     type        = string
+}
+
+variable "vpc" {
+    description = "VPC for GKE"
+    type        = string
+    default     = "google_compute_network.helloweb3-vpc"
+}
+
+variable "subnet" {
+    description = "Subnet for GKE"
+    type        = string
+    default     = "google_compute_subnetwork.helloweb3-subnet"
 }
