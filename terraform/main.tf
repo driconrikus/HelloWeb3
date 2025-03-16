609dd76314fd4d7009724eb1af1acc0a4969ed1e
Ricardo Valdez <rickondrius@gmail.com>
2025-03-15 21:01:56 -0400

    Added GKE module

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,12 @@ module "vpc" {
     source = "./modules/vpc"
     region = var.region
 }
+
+module "gke" {
+  source       = "./modules/gke"
+  cluster_name = "my-gke-cluster"
+  region       = "us-central1"
+  vpc_network  = module.vpc.vpc_id       # Pass VPC ID from the VPC module
+  subnet_name  = module.vpc.subnet_name  # Pass subnet name from the VPC module
+  machine_type = "e2-medium"
+}
