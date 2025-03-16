20fdad7e9f00a9498548572810814c98e7df8f74
Ricardo Valdez <rickondrius@gmail.com>
2025-03-15 21:01:37 -0400

    Added outputs.tf to vpc module to reference in gke module

---
+++ terraform/modules/gke/variables.tf
@@ -0,0 +1,22 @@
+variable "region" {
+  type = string
+  default = "us-central1"
+}
+
+variable "vpc_network" {
+  type = string
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "machine_type" {
+  type    = string
+  default = "e2-medium"
+}
+
+variable "node_count" {
+  type    = number
+  default = 1
+}
